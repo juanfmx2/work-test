f9e4dd8be034700a2021df196e4de0b1c762d388
juanfmx2 <juanfmx2@gmail.com>
2016-02-18 00:03:32 -0600

    jdbc mysql connector

--- fermilab/gratia-migration/logstash/run_logstash.sh
+++ fermilab/gratia-migration/logstash/run_logstash.sh
@@ -1,3 +1,3 @@
-export $LOGSTASH_PATH = '~/logstash-2.2.1'
-export $LOGSTASH_CONF = './master_summary.conf'
+LOGSTASH_PATH=~/logstash-2.2.1
+LOGSTASH_CONF=./master_summary.conf
 $LOGSTASH_PATH/bin/logstash -f $LOGSTASH_CONF
